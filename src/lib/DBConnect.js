163b816f3e400b2392321feb703d4c741d99eb23
ratulislam46 <arfanratul46@gmail.com>
2025-08-22 23:03:11 +0600

    inviroment variable access

--- src/lib/DBConnect.js
+++ src/lib/DBConnect.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 export const DBConnect = (collectionName) => {
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ibgq1ve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const uri = `mongodb+srv://${process.env.NEXT_PUBLIC_DB_USER}:${process.env.NEXT_PUBLIC_DB_PASS}@cluster0.ibgq1ve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
     const client = new MongoClient(uri, {
         serverApi: {
@@ -11,5 +11,5 @@ export const DBConnect = (collectionName) => {
             deprecationErrors: true,
         }
     });
-    return client.db(process.env.DB_NAME).collection(collectionName)
+    return client.db(process.env.NEXT_PUBLIC_DB_NAME).collection(collectionName)
 }
