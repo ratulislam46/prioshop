9f72050ac1b0aac9fbca5ce3188fd428cea3b915
ratulislam46 <arfanratul46@gmail.com>
2025-08-22 12:48:27 +0600

    add product form

--- src/app/addproduct/page.js
+++ src/app/addproduct/page.js
@@ -3,31 +3,38 @@ import React from 'react';
 
 const AddProduct = () => {
 
-    const handleAddProduct = () =>{
+    const handleAddProduct = (e) => {
         e.preventDefault()
         const form = e.target;
         const name = form.name.value;
         const price = form.price.value;
-        const describtion= form.name.value;
+        const quantity = form.quantity.value;
+        const description = form.description.value;
+        
     }
-    
+
     return (
         <div className='container mx-auto'>
             <section className='flex justify-center items-center'>
                 <form onSubmit={handleAddProduct}
-                className='p-4 border border-gray-300 rounded-xl my-24'
+                    className='p-4 border border-gray-300 rounded-xl my-24 shadow-xl hover:shadow-2xl'
                 >
                     <legend className="fieldset-legend text-black ">What is your product name?</legend>
-                    <input type="text" className="input bg-white" placeholder="Product Name" required />
-                    <legend className="fieldset-legend text-black ">What is your product price?</legend>
-                    <input type="text" className="input bg-white" placeholder="Product Price" required />
+                    <input type="text" name='name' className="input bg-white" placeholder="Product Name" required />
+
                     <legend className="fieldset-legend text-black ">Product image</legend>
-                    <input type="file" className="input bg-white pt-2 course-pointer" placeholder="Product Name" required />
+                    <input type="file" name='image' className="input bg-white pt-2 course-pointer" placeholder="Product Name" required />
+
                     <legend className="fieldset-legend text-black ">Product price</legend>
-                    <input type="number" className="input bg-white" placeholder="Product Name" min={10} required />
+                    <input type="number" name='price' className="input bg-white" placeholder="Product Name" min={1} required />
+
+                    <legend className="fieldset-legend text-black ">Product Quantity</legend>
+                    <input type="number" name='quantity' className="input bg-white" placeholder="Product Quantity" min={1} required />
+
                     <legend className="fieldset-legend text-black ">Product Description</legend>
-                    <input type="text" className="input bg-white" placeholder="Description" required /><br/><br/>
-                    <input value='Add Product' className='btn w-full'></input>
+                    <input type="text" name='description' className="input bg-white" placeholder="Description" required /><br /><br />
+
+                    <button type='submit' className='btn w-full'>Add Product</button>
                 </form>
             </section>
         </div>
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Products = () => {
+const Products = async () => {
+
+    const res = await fetch('http://localhost:3000/api/items');
+    const data = await res.json()
+
     return (
         <div>
             <h1>Products Page</h1>
+            <p>{JSON.stringify(data)}</p>
         </div>
     );
 };
 
-export default Products;<h1>Products Page</h1>
+export default Products; <h1>Products Page</h1>
